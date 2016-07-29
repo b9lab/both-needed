ec683d11cd9ad7323ea52aa10126227144e0ec0b
Xavier Lepretre <xavier.lepretre@gmail.com>
2016-07-29 15:06:24 +0100

    Split contract.

---
+++ contracts/MultiOwned.sol
@@ -0,0 +1,46 @@
+contract MultiOwned {
+	mapping (address => bool) public owners;
+	struct Confirmation {
+		uint count;
+		mapping (address => bool) confirmed;	
+	}
+	mapping (bytes32 => Confirmation) public confirmations;
+
+	event OnUnfinishedConfirmation(bytes32 key);
+	
+	function MultiOwned(address _owner2) {
+		owners[msg.sender] = true;
+		owners[_owner2] = true;
+	}
+
+	modifier fromOwner {
+		if (!owners[msg.sender]) {
+			throw;
+		}
+		_
+	}
+
+	modifier isConfirmed {
+		bytes32 key = sha3(msg.data);
+		if (!confirmations[key].confirmed[msg.sender]) {
+			confirmations[key].count++;
+		}
+		confirmations[key].confirmed[msg.sender] = true;
+		if (confirmations[key].count < 2) {
+			OnUnfinishedConfirmation(key);
+			return;
+		}
+		delete confirmations[key];
+		_
+	}
+
+	function changeOwner(address oldOwner, address newOwner) 
+		fromOwner
+		isConfirmed 
+		returns (bool success){
+		owners[oldOwner] = false;
+		owners[newOwner] = true;
+		return true;
+	}
+
+}
--- contracts/StringStore.sol
+++ contracts/StringStore.sol
@@ -1,47 +1,10 @@
-contract StringStore {
-	mapping (address => bool) public owners;
+import "MultiOwned.sol";
+
+contract StringStore is MultiOwned {
 	mapping (uint => string) public stored;
-	struct Confirmation {
-		uint count;
-		mapping (address => bool) confirmed;	
-	}
-	mapping (bytes32 => Confirmation) public confirmations;
 
-	event OnUnfinishedConfirmation(bytes32 key);
-
-	function StringStore(address _owner2) {
-		owners[msg.sender] = true;
-		owners[_owner2] = true;
-	}
-
-	modifier fromOwner {
-		if (!owners[msg.sender]) {
-			throw;
-		}
-		_
-	}
-
-	modifier isConfirmed {
-		bytes32 key = sha3(msg.data);
-		if (!confirmations[key].confirmed[msg.sender]) {
-			confirmations[key].count++;
-		}
-		confirmations[key].confirmed[msg.sender] = true;
-		if (confirmations[key].count < 2) {
-			OnUnfinishedConfirmation(key);
-			return;
-		}
-		delete confirmations[key];
-		_
-	}
-
-	function changeOwner(address oldOwner, address newOwner) 
-		fromOwner
-		isConfirmed 
-		returns (bool success){
-		owners[oldOwner] = false;
-		owners[newOwner] = true;
-		return true;
+	function StringStore(address _owner2)
+		MultiOwned(_owner2) {
 	}
 
 	function store(uint key0, string value) 
